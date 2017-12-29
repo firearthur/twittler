f298b0280cee589ecb8b4830ce1fcc82fb50072c
fireArthur <thefirearthur@gmail.com>
2017-12-29 12:42:15 -0800

    Added update buttons to index.html Styled the buttons at style.css Implemented update autoupdate and stop autoupdate features at app.js

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 $(document).ready(function() {
 
-  // renderTweets();
+  var tweetsUpdateNav = $('#tweets-update');
+  var renderTweetsInterval;
+
+  tweetsUpdateNav.on('click', function(event){
+    // event.stopPropagation();
+    if(event.target.name === 'update'){
+      clearInterval(renderTweetsInterval);
+      renderTweets();
+      console.log('manual update');
+
+    } else if(event.target.name === 'start-autoupdate'){
+      renderTweetsInterval = setInterval(renderTweets, 5000);
+      console.log('autoupdate');
+
+    } else if(event.target.name === 'stop-autoupdate'){
+      clearInterval(renderTweetsInterval);
+      console.log('stopped autoupdate');
+
+    }
+  });
+
+
+
+
+
+  // var renderTweetsInterval = setInterval(renderTweets, 10000);
+   renderTweets();
+  //  setTimeout(renderTweets, 3000);
+  //  setTimeout(renderTweets, 3000);
+
+
+
 
 
   function renderTweets(){
-
+    console.log('updated');
     var $section = $('section');
+    $section.html('');
     var index = streams.home.length - 1;
 
     while(index >= 0){
