662b73350f37d75f2fc1bb0f9caaff992480e68b
fireArthur <thefirearthur@gmail.com>
2017-12-29 10:47:11 -0800

    Refactored the tweet render process at app.js and removed some comments at index.html

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 $(document).ready(function() {
-    // var refresher = setInterval(update_content, 3000);
 
-})
+  // renderTweets();
 
-function update_content(){
 
-    $.ajax({
-      type: "GET",
-      url: "index.html",
-      cache: false,
-    })
-      .done(function( page_html ) {
-        console.log("LOADED");
-    var newDoc = document.open("text/html", "replace");
-    newDoc.write(page_html);
-    newDoc.close();
+  function renderTweets(){
 
-    });
+    var $section = $('section');
+    var index = streams.home.length - 1;
 
-}
+    while(index >= 0){
+      var $article = $('<article class = "tweet"></article>');
+      var tweet = streams.home[index];
+      var username = tweet.user;
+      var timestamp = tweet.created_at.toLocaleString();
+      var $timestamp = $('<small class="timestamp">Posted on: ' + timestamp +'</small>');
+      var $tweet = $('<p><a href = "#" class = "username" data-username = "'+ username + '">@'+ username +'</a>: ' + tweet.message + '</p>');
+      $tweet.appendTo($article);
+      $tweet.after($timestamp);
+      $article.appendTo($section);
+      index -= 1;
+    }
+  }
+});
